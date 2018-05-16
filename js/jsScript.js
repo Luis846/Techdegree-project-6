03aff211c09fd894ce854601c3777ae2daa46b0d
Luis846 <luis.rivera846@gmail.com>
2018-05-16 13:39:12 -0400

    updated

--- js/jsScript.js
+++ js/jsScript.js
@@ -1,15 +1,13 @@
+myVid = document.getElementById("theVid");
+spans = document.getElementsByClassName('Sp');
 
-  myVid = document.getElementById("theVid");
-  spans = document.getElementsByClassName('Sp');
-
-  myVid.ontimeupdate = () => {
-
-    for(i = 0; i < spans.length; i++){
-      if (myVid.currentTime >= spans[i].dataset.start) {
-            spans[i].classList.add('MediaText');
+myVid.ontimeupdate = () => {
+  for(i = 0; i < spans.length; i++){
+    if (myVid.currentTime >= spans[i].dataset.start) {
+        spans[i].classList.add('MediaText');
           }
-      if (myVid.currentTime >= spans[i].dataset.end) { 
-            spans[i].classList.remove('MediaText');
-          }
-       };
-    }
+    if (myVid.currentTime >= spans[i].dataset.end) {
+        spans[i].classList.remove('MediaText');
+      }
+    };
+}
